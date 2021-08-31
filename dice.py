a54187f166f7cf0b3ca2fd7e09554e97f5fd0f11
Bartosz Borek <65012705+NeverPlayFair@users.noreply.github.com>
2021-08-31 21:34:22 +0200

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,46 @@
+import random
+
+print("Welcome in my dice game and enjoy! ")
+
+x = "c"
+
+while x == "c":
+    number = random.randint(1,6)
+
+    if number == 1:
+        print("-----------")
+        print("|         |")
+        print("|    0    |")
+        print("|         |")
+        print("-----------")
+    if number == 2:
+        print("-----------")
+        print("|         |")
+        print("| 0     0 |")
+        print("|         |")
+        print("-----------")
+    if number == 3:
+        print("-----------")
+        print("|    0    |")
+        print("|    0    |")
+        print("|    0    |")
+        print("-----------")
+    if number == 4:
+        print("-----------")
+        print("| 0     0 |")
+        print("|         |")
+        print("| 0     0 |")
+        print("-----------")
+    if number == 5:
+        print("-----------")
+        print("| 0     0 |")
+        print("|    0    |")
+        print("| 0     0 |")
+        print("-----------")
+    if number == 6:
+        print("-----------")
+        print("| 0     0 |")
+        print("| 0     0 |")
+        print("| 0     0 |")
+        print("-----------")
+    x  = input("Press c if u want play again ")
